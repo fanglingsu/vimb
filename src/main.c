ac18d42915bd633b8d2678019950f15f5ee8894a
Daniel Carl <danielcarl@gmx.de>
2013-10-22 21:07:46 +0200

    Fixed error if there is no title set in update_title.

--- src/main.c
+++ src/main.c
@@ -868,15 +868,19 @@ static void update_title(void)
 #ifdef FEATURE_TITLE_PROGRESS
     /* show load status of page or the downloads */
     if (vb.state.progress != 100) {
-        char *title = g_strdup_printf("[%i%%] %s", vb.state.progress, vb.state.title);
+        char *title = g_strdup_printf(
+            "[%i%%] %s",
+            vb.state.progress,
+            vb.state.title ? vb.state.title : ""
+        );
         gtk_window_set_title(GTK_WINDOW(vb.gui.window), title);
         g_free(title);
-    } else {
+        return;
+    }
+#endif
+    if (vb.state.title) {
         gtk_window_set_title(GTK_WINDOW(vb.gui.window), vb.state.title);
     }
-#else
-    gtk_window_set_title(GTK_WINDOW(vb.gui.window), vb.state.title);
-#endif
 }
 
 static gboolean mimetype_decision_cb(WebKitWebView *webview,
