732a56f6b2125706e0cc5e305693d7e3d11465b9
Daniel Carl <danielcarl@gmx.de>
2014-12-04 23:16:44 +0100

    Don't put remote command into command history.

--- src/io.c
+++ src/io.c
@@ -101,8 +101,14 @@ static gboolean fifo_watch(GIOChannel *gio, GIOCondition condition)
         g_error_free(err);
     }
 
+    /* don't recore remote commands in history */
+    vb.state.enable_history = false;
+
     map_handle_string(line, true);
     g_free(line);
+
+    /* reenable history recording */
+    vb.state.enable_history = true;
 
     return true;
 }
