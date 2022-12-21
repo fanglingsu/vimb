8b85f9856797a6f8a0ba9817489bb4e185849b29
Patrick Steinhardt <ps@pks.im>
2022-12-21 15:47:15 +0100

    config.mk: link against webkit-gtk 4.1
    The webkit-gtk project has released a new minor version bump of their
    API with version 4.1. The difference between 4.0 and 4.1 is that the
    former links against libsoup 2.x, whereas the latter links against
    libsoup 3.0 and newer. As libsoup has introduced backwards-incompatible
    changes in 3.0 it will cause the process to hard-crash if a program is
    linked against both libraries at the same time.
    
    Now that we have ported vimb to be compatible with libsoup 3.0 we can
    start to link against the recent webkit-gtk 4.1.

--- config.mk
+++ config.mk
@@ -17,7 +17,7 @@ SRCDIR  = src
 DOCDIR  = doc
 
 # used libs
-LIBS = gtk+-3.0 'webkit2gtk-4.0 >= 2.20.0'
+LIBS = gtk+-3.0 webkit2gtk-4.1
 
 # setup general used CFLAGS
 CFLAGS   += -std=c99 -pipe -Wall -fPIC
@@ -34,9 +34,9 @@ endif
 
 # flags used to build webextension
 EXTTARGET   = webext_main.so
-EXTCFLAGS   = ${CFLAGS} $(shell pkg-config --cflags webkit2gtk-web-extension-4.0)
+EXTCFLAGS   = ${CFLAGS} $(shell pkg-config --cflags webkit2gtk-web-extension-4.1)
 EXTCPPFLAGS = $(CPPFLAGS)
-EXTLDFLAGS  = ${LDFLAGS} $(shell pkg-config --libs webkit2gtk-web-extension-4.0) -shared
+EXTLDFLAGS  = ${LDFLAGS} $(shell pkg-config --libs webkit2gtk-web-extension-4.1) -shared
 
 # flags used for the main application
 CFLAGS     += $(shell pkg-config --cflags $(LIBS))
